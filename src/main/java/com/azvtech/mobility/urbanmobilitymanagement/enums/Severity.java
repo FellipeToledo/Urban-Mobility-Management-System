d3c8fbc4d655c90ff75f21c691be8620bd6d1af9
FellipeToledo <fellipetoledo.dev@gmail.com>
2024-10-11 17:05:28 -0300

    Versão refatorada da classe Event com alguns aprimoramentos

---
+++ src/main/java/com/azvtech/mobility/urbanmobilitymanagement/enums/Severity.java
@@ -0,0 +1,8 @@
+package com.azvtech.mobility.urbanmobilitymanagement.enums;
+
+public enum Severity {
+    LOW,
+    MEDIUM,
+    HIGH,
+    CRITICAL
+}
--- src/main/java/com/azvtech/mobility/urbanmobilitymanagement/model/Event.java
+++ src/main/java/com/azvtech/mobility/urbanmobilitymanagement/model/Event.java
@@ -1,29 +1,51 @@
 package com.azvtech.mobility.urbanmobilitymanagement.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
+import com.azvtech.mobility.urbanmobilitymanagement.enums.Severity;
+import com.azvtech.mobility.urbanmobilitymanagement.enums.Status;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
-@Setter
-@Getter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
+@Table(name = "events")
 public class Event {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String description;
-    private LocalDateTime dateTime;
+
+    @Column(name = "registration_date_time", nullable = false)
+    private LocalDateTime registrationDateTime;
+
+    @Column(name = "start_date_time", nullable = false)
+    private LocalDateTime startDateTime;
+
+    @Column(name = "end_date_time")
+    private LocalDateTime endDateTime;
+
+    @Column(nullable = false)
     private String location;
-    private String severity;
-    private String status;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Severity severity;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status;
+
+    @Column(name = "impact_transit")
     private String impactTransit;
-    private boolean roadblock;
+
+    @Column(name = "road_block")
+    private boolean roadBlock;
 
 }
