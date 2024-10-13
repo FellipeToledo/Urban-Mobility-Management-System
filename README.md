5e243e198799033e1261df276f1182faee4ea3a2
FellipeToledo <fellipetoledo.dev@gmail.com>
2024-10-13 18:01:28 -0300

    feat: Documentação da API com Swagger

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# Urban-Mobility-Management-System
-O Urban Mobility Management System (UMMS) é uma solução inovadora voltada para a gestão de dados sobre a mobilidade urbana, com foco em eventos que causam impacto direto ou indireto no tráfego.
+<h1 align="center">Urban Mobility Management System</h1>
+
+----
+<p align="center">
+<a href="https://github.com/FellipeToledo/Urban-Mobility-Management-System">
+  <img src="https://raw.githubusercontent.com/FellipeToledo/files/refs/heads/main/u-m-m-high-resolution-logo-transparent.png" alt="umms-logo" width="200px" height="60px"/></a>
+  <br>
+  <em>O Urban Mobility Management System é uma plataforma que gerencia eventos de trânsito em tempo real
+    <br> otimizando o tráfego e melhorando a mobilidade urbana.</em>
+  <br>
+</p>
+
+<p align="center">
+  <a href="#Key-features">Key Features</a> •
+  <a href="#how-to-use">How To Use</a> •
+  <a href="#credits">Credits</a> •
+  <a href="#related">Related</a> •
+  <a href="#license">License</a>
+</p>
+
+---
+
+<div align="center">
+
+
+
+![screenshot](https://raw.githubusercontent.com/FellipeToledo/files/refs/heads/main/github-desktop.svg) [@FellipeToledo](https://github.com/FellipeToledo) &nbsp;&middot;&nbsp;   ![screenshot](https://raw.githubusercontent.com/FellipeToledo/files/refs/heads/main/linkedin-outlined.svg) [@FellipeToledo](https://www.linkedin.com/in/fellipetoledo/) &nbsp;&middot;&nbsp;
+
+</div>
--- src/main/java/com/azvtech/mobility/urbanmobilitymanagement/controller/EventController.java
+++ src/main/java/com/azvtech/mobility/urbanmobilitymanagement/controller/EventController.java
@@ -2,6 +2,10 @@ package com.azvtech.mobility.urbanmobilitymanagement.controller;
 
 import com.azvtech.mobility.urbanmobilitymanagement.model.Event;
 import com.azvtech.mobility.urbanmobilitymanagement.service.EventService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,7 +23,12 @@ public class EventController {
         this.eventService = eventService;
     }
 
-    @GetMapping
+    @Operation(summary = "Buscar todos os eventos")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Lista de eventos recuperada com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Nenhum evento encontrado")
+    })
+    @GetMapping("/events")
     public ResponseEntity<List<Event>> getAllEvents() {
         List<Event> events = eventService.findAllEvents();
         return events.isEmpty()
@@ -27,15 +36,29 @@ public class EventController {
                 : ResponseEntity.ok(events);
     }
 
+    @Operation(summary = "Buscar um evento pelo ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Evento encontrado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Evento não encontrado")
+    })
     @GetMapping("/{id}")
-    public ResponseEntity<Event> getEventById(@PathVariable Long id) {
+    public ResponseEntity<Event> getEventById(
+            @Parameter(description = "ID do evento a ser buscado")
+            @PathVariable Long id) {
         return eventService.findEventById(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    @Operation(summary = "Criar um novo evento")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Evento criado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Dados inválidos para criar o evento")
+    })
     @PostMapping
-    public ResponseEntity<Event> createEvent(@Valid @RequestBody Event event) {
+    public ResponseEntity<Event> createEvent(
+            @Valid
+            @RequestBody Event event) {
         Event newEvent = eventService.createEvent(event);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
@@ -43,15 +66,31 @@ public class EventController {
                 .body(newEvent);
     }
 
+    @Operation(summary = "Atualizar um evento existente")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Evento atualizado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Evento não encontrado")
+    })
     @PutMapping("/{id}")
-    public ResponseEntity<Event> updateEvent(@PathVariable Long id, @Valid @RequestBody Event updatedEvent) {
+    public ResponseEntity<Event> updateEvent(
+            @Parameter(description = "ID do evento a ser atualizado")
+            @PathVariable Long id,
+            @Valid
+            @RequestBody Event updatedEvent) {
         return eventService.updateEvent(id, updatedEvent)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    @Operation(summary = "Deletar um evento")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Evento deletado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Evento não encontrado")
+    })
     @DeleteMapping("/{id}")
-    public ResponseEntity<HttpStatus> deleteEvent(@PathVariable Long id) {
+    public ResponseEntity<HttpStatus> deleteEvent(
+            @Parameter(description = "ID do evento a ser deletado")
+            @PathVariable Long id) {
         return eventService.deleteEvent(id) ?
                 ResponseEntity.noContent().build() :
                 ResponseEntity.notFound().build();
