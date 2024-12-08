603e9f0683bb07e65353a27f9c408c117391fe4a
FellipeToledo <fellipetoledo.dev@gmail.com>
2024-12-08 11:34:52 -0300

    feat: Adicionar estrutura da classe GlobalExceptionHandler e implementar o manipulador MethodArgumentNotValidException

---
+++ event_service/src/main/java/com/azvtech/event_service/exception/ErrorResponse.java
@@ -0,0 +1,27 @@
+package com.azvtech.event_service.exception;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+/**
+ * @author Fellipe Toledo
+ */
+@Getter
+@Setter
+public class ErrorResponse {
+
+    private int status;
+    private String message;
+    private Map<String, String> errors;
+    private LocalDateTime timestamp;
+
+    public ErrorResponse(int status, String message, Map<String, String> errors) {
+        this.status = status;
+        this.message = message;
+        this.errors = errors;
+        this.timestamp = LocalDateTime.now();
+    }
+}
